c19298c48ce89b2abedac84ea3c114a197d07d4c
Neeraj Malwal <neerajmalwal10@gmail.com>
2023-02-08 00:19:43 +0530

    fix: version no adjusted

--- inc/theme-update/class-astra-theme-background-updater.php
+++ inc/theme-update/class-astra-theme-background-updater.php
@@ -110,7 +110,6 @@ if ( ! class_exists( 'Astra_Theme_Background_Updater' ) ) {
 			),
 			'4.0.2' => array(
 				'astra_theme_background_updater_4_0_2',
-				'astra_migrate_old_content_layouts'
 			),
 			'4.0.3' => array(
 				'astra_migrate_old_content_layouts'
