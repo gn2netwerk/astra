dd63ff69bc40c920d2dd2028c484feea8ab4a30e
Navanath Bhosale <navanathb@bsf.io>
2023-03-03 12:32:55 +0530

    fix: heart svg  updated with their size manipulation as well

--- admin/assets/build/dashboard-app.asset.php
+++ admin/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '89bbc424488243a42c09');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '17198f0725989ad5b302');
