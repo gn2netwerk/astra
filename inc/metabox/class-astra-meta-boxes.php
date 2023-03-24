d9bae6f4a585f7dcf7ca7d76dac4e2d4291cb772
Navanath Bhosale <navanathb@bsf.io>
2023-03-24 16:36:34 +0530

    chore: ignored deprecation notice for FILTER_SANITIZE_STRING

--- inc/metabox/class-astra-meta-boxes.php
+++ inc/metabox/class-astra-meta-boxes.php
@@ -406,6 +406,7 @@ if ( ! class_exists( 'Astra_Meta_Boxes' ) ) {
 
 					default:
 					case 'FILTER_SANITIZE_STRING':
+							// phpcs:ignore WordPress.PHP.NoSilencedErrors.Discouraged -- This deprecation will be addressed later.
 							$meta_value = filter_input( INPUT_POST, $key, FILTER_SANITIZE_STRING );
 						break;
 
