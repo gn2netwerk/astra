1d70fc8c0fe23f7e3ddf8c3c944342e3a27bd344
Neeraj Malwal <neerajmalwal10@gmail.com>
2023-04-09 18:35:10 +0530

    wip: migration cases

--- inc/theme-update/class-astra-theme-background-updater.php
+++ inc/theme-update/class-astra-theme-background-updater.php
@@ -110,8 +110,6 @@ if ( ! class_exists( 'Astra_Theme_Background_Updater' ) ) {
 			),
 			'4.0.2' => array(
 				'astra_theme_background_updater_4_0_2',
-			),
-			'4.0.3' => array(
 				'astra_migrate_content_layouts'
 			),
 		);
