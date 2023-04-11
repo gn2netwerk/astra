05ec25c963be545384f49857d7377d09f6e9e728
Neeraj Malwal <neerajmalwal10@gmail.com>
2023-04-11 15:33:38 +0530

    fix: resolve merge conflicts with next-release

--- assets/js/unminified/customizer-preview.js
+++ assets/js/unminified/customizer-preview.js
@@ -995,19 +995,6 @@ function hasWordPressWidgetBlockEditor() {
 		} );
 	} );
 
-
-	wp.customize( 'astra-settings[new-site-content-layout]', function( value ) {
-		value.bind( function( layout ) {
-			wp.customize.preview.send( 'refresh' );
-		} );
-	} );
-
-	wp.customize( 'astra-settings[site-content-layout]', function( value ) {
-		value.bind( function( layout ) {
-			wp.customize.preview.send( 'refresh' );
-		} );
-	} );
-
 	/**
 	 * Header Bottom Border color
 	 */
