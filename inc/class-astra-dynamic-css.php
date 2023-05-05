4d2ce700b2b764a6e9a0bd560110e05ad14dcd20
Neeraj Malwal <neerajmalwal10@gmail.com>
2023-05-05 15:44:48 +0530

    chore: UI conflict can be resolved through Blog Spacing option, therefore reversing commit.

--- inc/class-astra-dynamic-css.php
+++ inc/class-astra-dynamic-css.php
@@ -4291,20 +4291,6 @@ if ( ! class_exists( 'Astra_Dynamic_CSS' ) ) {
 				';
 			}
 
-			/** @psalm-suppress UndefinedClass */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
-			if ( defined( 'ASTRA_EXT_VER' ) && Astra_Ext_Extension::is_active( 'woocommerce' ) && astra_get_option( 'cart-modern-layout' ) ) {
-				$sidebar_static_css .= '
-				@media (min-width: 993px) {
-					.ast-right-sidebar #primary {
-						padding-' . $ltr_right . ': 20px;
-					}
-					.ast-left-sidebar #primary {
-						padding-' . $ltr_left . ': 20px;
-					}
-				}
-				';
-			}
-
 			return $sidebar_static_css;
 		}
 
