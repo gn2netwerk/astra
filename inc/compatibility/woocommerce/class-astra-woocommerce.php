5cb6fe6783c1b4ba76756bf1eeee3bb5115076bd
DR_GURUNG <racingfoxs@gmail.com>
2023-05-09 17:29:01 +0530

    AST-3165 FIX: Adjusted the width value

--- inc/compatibility/woocommerce/class-astra-woocommerce.php
+++ inc/compatibility/woocommerce/class-astra-woocommerce.php
@@ -2887,7 +2887,7 @@ if ( ! class_exists( 'Astra_Woocommerce' ) ) :
 
 					.ast-single-product-payments ul li {
 						display: flex;
-						width: 50px;
+						width: 48px;
 						margin: 0 0.5em 0.5em 0.5em;
 					}
 
