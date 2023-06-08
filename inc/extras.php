3e001bb23067c838e5ba4c9e221171ade0bec2f4
Neeraj Malwal <neerajmalwal10@gmail.com>
2023-06-08 14:58:36 +0530

    fix: adding isset checks.

--- inc/extras.php
+++ inc/extras.php
@@ -194,16 +194,19 @@ if ( ! function_exists( 'astra_toggle_layout' ) ) {
 		// Meta layout migrations.
 		$meta_old_layout = astra_get_option_meta('site-content-layout', '', true );
 		$meta_key        = astra_get_option_meta( 'astra-migrate-meta-layouts', '', true );
+		$migrated_user   = ( ! Astra_Dynamic_CSS::astra_fullwidth_sidebar_support() );
 
 		// Third party archive meta migration.
 		$third_party_meta_page = astra_third_party_archive_meta( 'site-content-layout' );
-		if ( '' === $meta_old_layout && isset( $third_party_meta_page ) && $third_party_meta_page ) {
+		if ( isset( $meta_old_layout ) && '' === $meta_old_layout && isset( $third_party_meta_page ) && $third_party_meta_page ) {
 			$meta_old_layout = $third_party_meta_page;
 			$meta_key = astra_third_party_archive_meta( 'astra-migrate-meta-layouts' );
 		}
 
-		if ( $meta_old_layout && 'set' !== $meta_key ) {
-			$dynamic_layout_option = astra_migrate_meta_layout( $meta_old_layout );
+		if ( isset( $meta_key ) && 'set' !== $meta_key && isset( $migrated_user ) && $migrated_user ) {
+			if ( isset( $meta_old_layout ) && $meta_old_layout ) {
+				$dynamic_layout_option = astra_migrate_meta_layout( $meta_old_layout );
+			}
 		}
 
 		switch ( $dynamic_layout_option ) {
@@ -214,7 +217,7 @@ if ( ! function_exists( 'astra_toggle_layout' ) ) {
 				$current_layout = 'narrow-container';
 
 				// Exclude narrow layout for third party cases.
-				if ( astra_is_third_party() && ( Astra_Dynamic_CSS::astra_fullwidth_sidebar_support() ) ) {
+				if ( astra_is_third_party() && isset( $migrated_user ) && $migrated_user ) {
 					$current_layout = 'plain-container';
 				}
 				break;
