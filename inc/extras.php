c591d219a3b6de829e0ea90a8db5e40d5162f508
yssambare12 <bloggingscan@gmail.com>
2023-06-14 09:28:45 +0530

    chore:fix extra.php files php compatibility issues

--- inc/extras.php
+++ inc/extras.php
@@ -642,9 +642,11 @@ function astra_load_preload_local_fonts( $url, $format = 'woff2' ) {
  * @since 3.6.0
  */
 function astra_get_transparent_header_default_value() {
-	$astra_settings                                      = get_option( ASTRA_THEME_SETTINGS );
-	$astra_settings['transparent-header-default-border'] = isset( $astra_settings['transparent-header-default-border'] ) ? false : true;
-	return apply_filters( 'astra_transparent_header_default_border', $astra_settings['transparent-header-default-border'] );
+    $astra_settings = get_option( ASTRA_THEME_SETTINGS, array() );
+
+    $astra_settings['transparent-header-default-border'] = isset( $astra_settings['transparent-header-default-border'] ) ? $astra_settings['transparent-header-default-border'] : array();
+
+    return apply_filters( 'astra_transparent_header_default_border', $astra_settings['transparent-header-default-border'] );
 }
 
 /**
