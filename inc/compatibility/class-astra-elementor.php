d91d59e29e7da239895bb8d763286664433b73d1
yssambare12 <bloggingscan@gmail.com>
2023-06-16 08:25:53 +0530

    chore:rearrange the logic and make it simplified

--- inc/compatibility/class-astra-elementor.php
+++ inc/compatibility/class-astra-elementor.php
@@ -82,25 +82,27 @@ if ( ! class_exists( 'Astra_Elementor' ) ) :
 		 *
 		 * @since 4.1.0
 		 */
-		function astra_entry_header_class_custom( $classes ) {
-			$post_id = astra_get_post_id();
-			/** @psalm-suppress InvalidArgument */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
-			$edit_mode = get_post_meta( $post_id, '_elementor_edit_mode', true );
+/**
+ * Hide elementor title.
+ *
+ * @param array $classes Array of elementor edit mode check.
+ *
+ * @since 4.1.0
+ */
+function astra_entry_header_class_custom( $classes ) {
+	$edit_mode = get_post_meta( astra_get_post_id(), '_elementor_edit_mode', true );
 
-			if ( $edit_mode && $edit_mode === 'builder' ) {
-				$classes[] = 'ast-header-without-markup';
+	if ( ( $edit_mode && $edit_mode === 'builder' ) || ( $edit_mode === 'builder' && $astra_layout_type == 'layout-2' ) ) {
+		$classes[] = 'ast-header-without-markup';
 
-				// Check layout type and show page title markup.
-				$astra_post_type      = strval( get_post_type() );
-				$astra_banner_control = 'ast-dynamic-single-' . esc_attr( $astra_post_type );
-				$astra_layout_type    = astra_get_option( $astra_banner_control . '-layout', 'layout-1' );
-				if ( $astra_layout_type == 'layout-2' && in_array( 'ast-header-without-markup', $classes ) ) {
-					unset( $classes[ array_search( 'ast-header-without-markup', $classes ) ] );
-				}
-			}
-
-			return $classes;
+		$astra_layout_type = astra_get_option( 'ast-dynamic-single-' . get_post_type() . '-layout', 'layout-1' );
+		if ( $astra_layout_type === 'layout-2' && in_array( 'ast-header-without-markup', $classes ) ) {
+			unset( $classes[ array_search( 'ast-header-without-markup', $classes ) ] );
 		}
+	}
+
+	return $classes;
+}
 
 		/**
 		 * Compatibility CSS for Elementor Headings after Elementor-v2.9.9
