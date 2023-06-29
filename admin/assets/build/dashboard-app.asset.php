a2af3aea4f5764799efffe7381e2d39d47cf4db7
Navanath Bhosale <navanathb@bsf.io>
2023-06-29 18:15:19 +0530

    improvement: Added a new filter 'astra_show_banner_video' to display/hide admin page banner YouTube video

--- admin/assets/build/dashboard-app.asset.php
+++ admin/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '1aa1c8170fb3b6b27c70');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '816e0699c1bb2f5c85fe');
