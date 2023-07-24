48b7bafe312c4ba6275114d5a33a98f27ae0a725
Yogesh <bloggingscan@gmail.com>
2023-07-24 17:06:45 +0530

    chore:corrected formatting

--- assets/js/unminified/sticky-add-to-cart.js
+++ assets/js/unminified/sticky-add-to-cart.js
@@ -1,12 +1,12 @@
-(function() {
+(function () {
 
     // Triggers sticky add to cart on scroll.
-    const astraStickyAddToCart = document.querySelector( ".ast-sticky-add-to-cart" );
+    const astraStickyAddToCart = document.querySelector(".ast-sticky-add-to-cart");
 
-    if( astraStickyAddToCart ) {
-        const scrollOffset = document.querySelector( '.product .single_add_to_cart_button' ).offsetTop;
-        window.addEventListener( "scroll", function() {
-            if ( window.scrollY >= scrollOffset ) {
+    if (astraStickyAddToCart) {
+        const scrollOffset = document.querySelector('.product .single_add_to_cart_button').offsetTop;
+        window.addEventListener("scroll", function () {
+            if (window.scrollY >= scrollOffset) {
                 astraStickyAddToCart.classList.add('is-active');
             } else {
                 astraStickyAddToCart.classList.remove('is-active');
@@ -15,20 +15,20 @@
     }
 
     // Smooth scrolls if select option button is active.
-    const AstraSmoothScrollBtn = document.querySelector( ".ast-sticky-add-to-cart-action-wrap .single_link_to_cart_button" );
+    const AstraSmoothScrollBtn = document.querySelector(".ast-sticky-add-to-cart-action-wrap .single_link_to_cart_button");
     const element = document.querySelector(".single_add_to_cart_button");
 
-    if( AstraSmoothScrollBtn && element ) {
-    var headerOffset = 200;
-    var elementPosition = element.getBoundingClientRect().top;
-    var offsetPosition = elementPosition + window.pageYOffset - headerOffset;
-    AstraSmoothScrollBtn.addEventListener('click', function (e) {
+    if (AstraSmoothScrollBtn && element) {
+        var headerOffset = 200;
+        var elementPosition = element.getBoundingClientRect().top;
+        var offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+        AstraSmoothScrollBtn.addEventListener('click', function (e) {
             e.preventDefault();
             window.scrollTo({
                 top: offsetPosition,
                 behavior: "smooth"
-           });
-  });
-}
+            });
+        });
+    }
 
 })();
