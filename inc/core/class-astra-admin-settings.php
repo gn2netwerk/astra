a54fa206e4a8471a664248aee050184ef8a20225
Neeraj Malwal <neerajm@bsf.io>
2023-07-25 16:59:10 +0530

    fix: static analysis

--- inc/core/class-astra-admin-settings.php
+++ inc/core/class-astra-admin-settings.php
@@ -153,7 +153,9 @@ if ( ! class_exists( 'Astra_Admin_Settings' ) ) {
 			if ( current_user_can( 'install_plugins' ) && ! defined( 'ASTRA_SITES_NAME' ) && '1' == get_option( 'fresh_site' ) && ! in_array( $current_slug, array( 'astra-advanced-hook', 'astra_adv_header' ), true ) ) {
 
 				// Do not display admin welcome banner notice on theme upload page.
+				/** @psalm-suppress InvalidGlobal */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
 				global $pagenow;
+				/** @psalm-suppress InvalidGlobal */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
 				if ( isset( $pagenow ) && 'update.php' === $pagenow ) {
 					return;
 				}
