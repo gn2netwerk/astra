c12bd53c7a70e9eb72c1832c1ba2c79fab19fa15
Navanath Bhosale <navanathb@bsf.io>
2023-08-07 16:20:29 +0530

    chore: PR suggession

--- inc/core/class-astra-enqueue-scripts.php
+++ inc/core/class-astra-enqueue-scripts.php
@@ -393,7 +393,6 @@ if ( ! class_exists( 'Astra_Enqueue_Scripts' ) ) {
 					'checkout_url'            => wc_get_checkout_url(),
 					'is_astra_pro'            => $is_astra_pro,
 				);
-			
 				wp_localize_script( 'astra-shop-add-to-cart', 'astra_shop_add_to_cart', apply_filters( 'astra_shop_add_to_cart_js_localize', $astra_shop_add_to_cart_localize_data ) );
 			}
 		}
