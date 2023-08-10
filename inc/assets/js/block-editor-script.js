c0b88fb223fcd022321def8595f3a373eadb7400
Neeraj Malwal <neerajm@bsf.io>
2023-08-10 15:57:53 +0530

    fix: editor outline compatibility with 6.3

--- inc/assets/js/block-editor-script.js
+++ inc/assets/js/block-editor-script.js
@@ -201,16 +201,18 @@ function astra_onload_function() {
 			}
 
 			// Show post/page title wrapper outline & eye icon only when clicked.
-			const titleInput     = document.querySelector('.editor-post-title__input');
-			const visibilityIcon = document.querySelector('.title-visibility');
+			const titleInput     = editorDocument.querySelector('.editor-post-title__input');
+			const visibilityIcon = editorDocument.querySelector('.title-visibility');
 			if( null != titleInput && null != visibilityIcon ) {
-				document.addEventListener('click', function (event){
-					if( ! titleBlock.contains( event.target ) ){
-						visibilityIcon.classList.remove('ast-show-visibility-icon');
-						titleInput.classList.remove('ast-show-editor-title-outline');
-					}
-				});
-				document.addEventListener('visibilitychange', function (){
+				if ( ! astraColors.ast_wp_version_higher_6_3 ) {
+					editorDocument.addEventListener('click', function (event){
+						if( ! titleBlock.contains( event.target ) ){
+							visibilityIcon.classList.remove('ast-show-visibility-icon');
+							titleInput.classList.remove('ast-show-editor-title-outline');
+						}
+					});
+				}
+				editorDocument.addEventListener('visibilitychange', function (){
 						visibilityIcon.classList.remove('ast-show-visibility-icon');
 						titleInput.classList.remove('ast-show-editor-title-outline');
 				});
