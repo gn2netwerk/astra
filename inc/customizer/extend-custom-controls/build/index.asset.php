e66cc8b73a13349b13b983a75eee7027d225a16a
Nathan Rodrigues <nathanrodrigues2111@gmail.com>
2023-08-14 13:56:25 +0530

    fix : values not unlinked when different values are selected

--- inc/customizer/extend-custom-controls/build/index.asset.php
+++ inc/customizer/extend-custom-controls/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-primitives'), 'version' => 'a7722921c303d86d93d373cfdc5f76b7');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-primitives'), 'version' => '26fb9ef4a5012bb0d128291874dad4a4');
