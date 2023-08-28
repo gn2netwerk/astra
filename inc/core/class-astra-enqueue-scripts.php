37f3150b6d82a6ead388230bec8e1bde2aad56ed
Neeraj Malwal <neerajm@bsf.io>
2023-08-28 19:25:25 +0530

    chore: supress psalm

--- inc/core/class-astra-enqueue-scripts.php
+++ inc/core/class-astra-enqueue-scripts.php
@@ -412,6 +412,11 @@ if ( ! class_exists( 'Astra_Enqueue_Scripts' ) ) {
 
 			$sticky_sidebar = astra_get_option( 'site-sticky-sidebar', false );
 			if ( $sticky_sidebar ) {
+
+				/** @psalm-suppress UndefinedClass */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
+				$sticky_header_addon = ( defined( 'ASTRA_EXT_VER' ) && Astra_Ext_Extension::is_active( 'sticky-header' ) );
+				/** @psalm-suppress UndefinedClass */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
+
 				$astra_sticky_sidebar_localize_data = array(
 					'sticky_sidebar_on'   => $sticky_sidebar,
 					'header_above_height' => astra_get_option( 'hba-header-height' ),
@@ -420,9 +425,8 @@ if ( ! class_exists( 'Astra_Enqueue_Scripts' ) ) {
 					'header_above_stick'  => astra_get_option( 'header-above-stick', false ),
 					'header_main_stick'   => astra_get_option( 'header-main-stick', false ),
 					'header_below_stick'  => astra_get_option( 'header-below-stick', false ),
-					/** @psalm-suppress UndefinedClass */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
-					'sticky_header_addon' => ( defined( 'ASTRA_EXT_VER' ) && Astra_Ext_Extension::is_active( 'sticky-header' ) ),
-					/** @psalm-suppress UndefinedClass */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
+					
+					'sticky_header_addon' => $sticky_header_addon,
 				);
 				wp_localize_script( 'astra-sticky-sidebar', 'astra_sticky_sidebar', apply_filters( 'astra_sticky_sidebar_js_localize', $astra_sticky_sidebar_localize_data ) );
 			}
