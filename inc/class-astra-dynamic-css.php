2911bc27683abcf5e05cfe0408d970952c4b5dad
Neeraj Malwal <neerajm@bsf.io>
2023-08-29 18:08:24 +0530

    chore: static analysis fixes

--- inc/class-astra-dynamic-css.php
+++ inc/class-astra-dynamic-css.php
@@ -4957,12 +4957,14 @@ if ( ! class_exists( 'Astra_Dynamic_CSS' ) ) {
 
 				/** @psalm-suppress PossiblyUndefinedVariable */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
 				$parse_css .= astra_parse_css(
+				/** @psalm-suppress PossiblyUndefinedVariable */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
 					$sidebar_sticky_css,
 					intval( $desktop_breakpoint ) + 1
 				);
 
 				/** @psalm-suppress PossiblyUndefinedVariable */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
 				$parse_css .= astra_parse_css(
+				/** @psalm-suppress PossiblyUndefinedVariable */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
 					$sidebar_webkit_sticky_css,
 					intval( $desktop_breakpoint ) + 1
 				);
@@ -4970,6 +4972,7 @@ if ( ! class_exists( 'Astra_Dynamic_CSS' ) ) {
 
 			/** @psalm-suppress PossiblyUndefinedVariable */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
 			return $parse_css;
+			/** @psalm-suppress PossiblyUndefinedVariable */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
 		}
 
 		/*
