8bcccad249e182ff692fba22acd146d2538e17b7
Navanath Bhosale <navanathb@bsf.io>
2023-08-31 11:21:52 +0530

    chore: PR intent suggession

--- inc/class-astra-dynamic-css.php
+++ inc/class-astra-dynamic-css.php
@@ -1761,7 +1761,10 @@ if ( ! class_exists( 'Astra_Dynamic_CSS' ) ) {
 			}
 
 
-			/* Add margin bottom to the figure element conditionally for WordPress 6.3 or above */
+			/**
+			 * Add margin-bottom to the figure element conditionally for WordPress 6.3 or above.
+			 * @since x.x.x
+			 */
 			if ( astra_wp_version_compare('6.3', '>=') ) {
 				$figure_margin_bottom = array(
 					'.entry-content[ast-blocks-layout] > figure' => array(
