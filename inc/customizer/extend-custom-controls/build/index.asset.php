ded7f6565b370e7d4da2e658235ab5d8c441ecbb
Navanath Bhosale <navanathb@bsf.io>
2023-09-06 14:29:13 +0530

    refactor: extended multi-selector control

--- inc/customizer/extend-custom-controls/build/index.asset.php
+++ inc/customizer/extend-custom-controls/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-primitives'), 'version' => '0c75fa3a07b1e99567fe0ccd4eae9719');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-primitives'), 'version' => 'e861e3a413067969c97881363ef418b9');
