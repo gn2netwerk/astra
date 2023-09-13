c45d654e2342df6f8c4469666bc1c6721cb8621c
Navanath Bhosale <navanathb@bsf.io>
2023-09-13 13:18:47 +0530

    refactor: pre-commit added for Astra

--- inc/class-astra-dynamic-css.php
+++ inc/class-astra-dynamic-css.php
@@ -1764,9 +1764,10 @@ if ( ! class_exists( 'Astra_Dynamic_CSS' ) ) {
 
 			/**
 			 * Add margin-bottom to the figure element conditionally for WordPress 6.3 or above.
+			 *
 			 * @since x.x.x
 			 */
-			if ( astra_wp_version_compare('6.3', '>=') ) {
+			if ( astra_wp_version_compare( '6.3', '>=' ) ) {
 				$figure_margin_bottom = array(
 					'.entry-content[ast-blocks-layout] > figure' => array(
 						'margin-bottom' => '1em',
@@ -4956,7 +4957,7 @@ if ( ! class_exists( 'Astra_Dynamic_CSS' ) ) {
 		public static function astra_sticky_sidebar_css() {
 			$css = '';
 			if ( astra_get_option( 'site-sticky-sidebar', false ) ) {
-				$sidebar_sticky_css = array(
+				$sidebar_sticky_css        = array(
 					'.ast-sticky-sidebar .sidebar-main' => array(
 						'top'        => '50px',
 						'position'   => 'sticky',
@@ -4965,7 +4966,7 @@ if ( ! class_exists( 'Astra_Dynamic_CSS' ) ) {
 				);
 				$sidebar_webkit_sticky_css = array(
 					'.ast-sticky-sidebar .sidebar-main' => array(
-						'position'   => '-webkit-sticky',
+						'position' => '-webkit-sticky',
 					),
 				);
 
