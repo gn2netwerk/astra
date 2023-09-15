cf2ddd4487f6f6e9153be53c9714e7400b2356df
Nathan Rodrigues <nathanrodrigues2111@gmail.com>
2023-09-15 17:11:23 +0530

    feat : added mult select control

--- inc/customizer/extend-custom-controls/src/index.js
+++ inc/customizer/extend-custom-controls/src/index.js
@@ -47,6 +47,7 @@ import { multiSelectorControl } from './multi-selector/control';
 import { astFontExtrasControl } from './ast-font-extras/control';
 import { astUpgradeControl } from './ast-upgrade/control';
 import { radioIconControl } from './ast-radio-icon/control';
+import { multiSelectControl } from './ast-select-multi/control';
 
 window.svgIcons = svgIcons;
 wp.customize.controlConstructor['ast-heading'] = headingControl;
@@ -96,6 +97,7 @@ wp.customize.controlConstructor['ast-upgrade'] = astUpgradeControl;
 wp.customize.controlConstructor['ast-font-extras'] = astFontExtrasControl;
 wp.customize.controlConstructor['ast-box-shadow'] = boxShadowControl;
 wp.customize.controlConstructor["ast-radio-icon"] = radioIconControl;
+wp.customize.controlConstructor["ast-select-multi"] = multiSelectControl;
 
 import { Base } from './customizer';
 import { ControlBase } from './control-customizer';
