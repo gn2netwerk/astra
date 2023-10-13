040244c19dc39b77bbe70dd84b4f128d1bb96995
Neeraj Malwal <neerajm@bsf.io>
2023-10-13 16:27:43 +0530

    fix: static analysis & phpcs

--- inc/addons/theme-builder/includes/class-astra-theme-builder-free.php
+++ inc/addons/theme-builder/includes/class-astra-theme-builder-free.php
@@ -24,7 +24,7 @@ if ( ! class_exists( 'Astra_Theme_Builder_Free' ) ) {
 		/**
 		 * Member Variable
 		 *
-		 * @var instance
+		 * @var null $instance
 		 */
 		private static $instance;
 
@@ -33,7 +33,9 @@ if ( ! class_exists( 'Astra_Theme_Builder_Free' ) ) {
 		 */
 		public static function get_instance() {
 			if ( ! isset( self::$instance ) ) {
+				/** @psalm-suppress InvalidPropertyAssignmentValue */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
 				self::$instance = new self();
+				/** @psalm-suppress InvalidPropertyAssignmentValue */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
 			}
 			return self::$instance;
 		}
@@ -42,51 +44,53 @@ if ( ! class_exists( 'Astra_Theme_Builder_Free' ) ) {
 		 *  Constructor
 		 */
 		public function __construct() {
+			/** @psalm-suppress UndefinedClass */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
 			$is_astra_addon_active = ( defined( 'ASTRA_EXT_VER' ) && Astra_Ext_Extension::is_active( 'advanced-hooks' ) );
+			/** @psalm-suppress UndefinedClass */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
 
 			if ( ! $is_astra_addon_active ) {
 				add_action( 'admin_enqueue_scripts', array( $this, 'theme_builder_admin_enqueue_scripts' ) );
 				add_action( 'admin_body_class', array( $this, 'admin_body_class' ) );
-				add_action( 'admin_menu', array( $this, 'setup_menu' ) );			
+				add_action( 'admin_menu', array( $this, 'setup_menu' ) );           
 			}
 		}
 
 		/**
 		 *  Enqueue scripts and styles.
-         * 
-         * @return void
+		 * 
+		 * @return void
 		 */
 		public function theme_builder_admin_enqueue_scripts() {
 			$file_prefix = '';
 			if ( is_rtl() ) {
 				$file_prefix .= '.rtl';
 			}
-            wp_enqueue_style( 'astra-theme-builder-style', ASTRA_THEME_BUILDER_FREE_URI . 'build/index' . $file_prefix . '.css', array(), ASTRA_THEME_VERSION );
-            wp_enqueue_script( 'astra-theme-builder-script', ASTRA_THEME_BUILDER_FREE_URI . 'build/index.js', array( 'wp-element' ), ASTRA_THEME_VERSION, true );
-			wp_enqueue_style('dashicons');
+			wp_enqueue_style( 'astra-theme-builder-style', ASTRA_THEME_BUILDER_FREE_URI . 'build/index' . $file_prefix . '.css', array(), ASTRA_THEME_VERSION );
+			wp_enqueue_script( 'astra-theme-builder-script', ASTRA_THEME_BUILDER_FREE_URI . 'build/index.js', array( 'wp-element' ), ASTRA_THEME_VERSION, true );
+			wp_enqueue_style( 'dashicons' );
 
 			$localized_data = array(
-				'title'  				 => __( 'Theme Builder', 'astra' ),
-				'astra_pricing_page_url' => "https://wpastra.com/pricing/",
-				'astra_docs_page_url'    => "https://wpastra.com/docs/",
+				'title'                  => __( 'Theme Builder', 'astra' ),
+				'astra_pricing_page_url' => 'https://wpastra.com/pricing/',
+				'astra_docs_page_url'    => 'https://wpastra.com/docs/',
 			);
 
 			wp_localize_script( 'astra-theme-builder-script', 'astra_theme_builder', $localized_data );
 		}
 
-        /**
-         * Admin Body Classes
-         *
-         * @since x.x.x
-         * @param string $classes Space separated class string.
-         */
-        public function admin_body_class( $classes = '' ) {
-            $theme_builder_class = isset( $_GET['page'] ) && 'theme-builder-free' === $_GET['page'] ? 'ast-theme-builder' : ''; // phpcs:ignore WordPress.Security.NonceVerification.Recommended -- Fetching a $_GET value, no nonce available to validate.
-            $classes .= ' ' . $theme_builder_class . ' ';
+		/**
+		 * Admin Body Classes
+		 *
+		 * @since x.x.x
+		 * @param string $classes Space separated class string.
+		 */
+		public function admin_body_class( $classes = '' ) {
+			$theme_builder_class = isset( $_GET['page'] ) && 'theme-builder-free' === $_GET['page'] ? 'ast-theme-builder' : ''; // phpcs:ignore WordPress.Security.NonceVerification.Recommended -- Fetching a $_GET value, no nonce available to validate.
+			$classes            .= ' ' . $theme_builder_class . ' ';
 
-            return $classes;
+			return $classes;
 
-        }
+		}
 
 		/**
 		 * Renders the admin settings.
