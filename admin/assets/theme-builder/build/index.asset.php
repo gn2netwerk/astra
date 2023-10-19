b67771754bc2dd797e408810196dfcff50f1f613
Neeraj Malwal <neerajm@bsf.io>
2023-10-20 00:27:22 +0530

    fix: sidebar responsiveness

--- admin/assets/theme-builder/build/index.asset.php
+++ admin/assets/theme-builder/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => '7b6ff03eaabea460365d');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => '61c829858b770e07362e');
