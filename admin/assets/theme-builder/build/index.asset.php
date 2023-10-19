f11867bdbb796d56f065e67cc08c7f98e847d721
Neeraj Malwal <neerajm@bsf.io>
2023-10-20 00:16:38 +0530

    fix: responsiveness

--- admin/assets/theme-builder/build/index.asset.php
+++ admin/assets/theme-builder/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => 'cfe3d320b62d7da1ffaf');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => '7b6ff03eaabea460365d');
