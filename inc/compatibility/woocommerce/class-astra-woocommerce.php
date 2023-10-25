4dd209cfc498e27ae6c5151b26bb03f7e171c42f
Sudhanshu-1508 <sudhanshukulkarni55@gmail.com>
2023-10-25 10:49:37 +0530

    fix: duplicate message for woo

--- inc/compatibility/woocommerce/class-astra-woocommerce.php
+++ inc/compatibility/woocommerce/class-astra-woocommerce.php
@@ -3377,7 +3377,7 @@ if ( ! class_exists( 'Astra_Woocommerce' ) ) :
 				?>
 					<div class="ast-mini-cart-empty">
 						<div class="ast-mini-cart-message">
-							<p class="woocommerce-mini-cart__empty-message"><?php echo esc_html( apply_filters( 'astra_mini_cart_empty_msg', __( 'No products in the cart.', 'astra' ) ) ); ?></p>
+						<p class="woocommerce-mini-cart__empty-message"><?php if ( !astra_get_option( 'woocommerce-sidebar-layout' ) ) { echo esc_html( apply_filters( 'astra_mini_cart_empty_msg', __( 'No products in the cart.', 'astra' ) ) ); } ?></p>
 						</div>
 						<?php do_action( 'astra_empty_cart_content' ); ?>
 						<div class="woocommerce-mini-cart__buttons">
