2258dad7f2dfd3d8bcda148dd60e5dba69f1d131
Neeraj Malwal <neerajm@bsf.io>
2023-10-25 17:05:35 +0530

    fix: rebranding custom layout as theme builder

--- admin/assets/build/dashboard-app.asset.php
+++ admin/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '6ede8e96a897bbd3e845');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '0763349948c84f0abb6c');
