b5324bdd63d3047c9915de761b1f4540eaafec33
Neeraj Malwal <neerajm@bsf.io>
2023-10-26 10:52:34 +0530

    fix: theme builder acceptance testing points

--- admin/assets/theme-builder/build/index.asset.php
+++ admin/assets/theme-builder/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => '6d9649edea7e9ee01667');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => 'b21e37a634fa0087fa4a');
