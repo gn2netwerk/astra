1d04821d23d4dd5930a8c76a4ee24aa84091922f
Navanath Bhosale <navanathb@bsf.io>
2023-10-30 17:00:02 +0530

    fix: pipeline issue

--- tests/e2e/specs/customizer/header-builder/header-types/mobile-header/off-canvas-menu/link-color.test.js
+++ tests/e2e/specs/customizer/header-builder/header-types/mobile-header/off-canvas-menu/link-color.test.js
@@ -3,41 +3,6 @@ import { setCustomize } from '../../../../../../utils/customize';
 import { setBrowserViewport } from '../../../../../../utils/set-browser-viewport';
 import { publishPost } from '../../../../../../utils/publish-post';
 describe( 'Off canvas menu link color settings in the customizer', () => {
-	it( 'link color should apply correctly for after header', async () => {
-		const offCanvasLinkSetting = {
-			'header-mobile-menu-color-responsive': {
-				tablet: 'rgb(133, 10, 158)',
-				mobile: 'rgb(6, 109, 16)',
-			},
-		};
-		await setCustomize( offCanvasLinkSetting );
-		let ppStatus = false;
-		while ( false === ppStatus ) {
-			await createNewPost( { postType: 'page', title: 'test-1' } );
-			ppStatus = await publishPost();
-			await createNewPost( { postType: 'page', title: 'test-2' } );
-			ppStatus = await publishPost();
-		}
-		await page.goto( createURL( '/' ), {
-			waitUntil: 'networkidle0',
-		} );
-		await setBrowserViewport( 'medium' );
-		await page.click( '.main-header-menu-toggle' );
-		await page.waitForSelector( '.ast-builder-menu-mobile .main-navigation .main-header-menu .menu-item > .menu-link' );
-		await expect( {
-			selector: '.ast-builder-menu-mobile .main-navigation .main-header-menu .menu-item > .menu-link',
-			property: 'color',
-		} ).cssValueToBe( `${ offCanvasLinkSetting[ 'header-mobile-menu-color-responsive' ].tablet }` );
-
-		await setBrowserViewport( 'small' );
-		await page.click( '.main-header-menu-toggle' );
-		await page.waitForSelector( '.ast-builder-menu-mobile .main-navigation .main-header-menu .menu-item > .menu-link' );
-		await expect( {
-			selector: '.ast-builder-menu-mobile .main-navigation .main-header-menu .menu-item > .menu-link',
-			property: 'color',
-		} ).cssValueToBe( `${ offCanvasLinkSetting[ 'header-mobile-menu-color-responsive' ].mobile }` );
-	} );
-
 	it( 'active link color should apply corectly for after header', async () => {
 		const offCanvasActiveLinkSetting = {
 			'header-mobile-menu-a-color-responsive': {
