0adf78434ddc498aadc571e07b33db18a09da8d6
Neeraj Malwal <neerajm@bsf.io>
2023-11-14 14:58:29 +0530

    fix: sonarqube comments

--- admin/assets/theme-builder/src/ThemeBuilderRouter.js
+++ admin/assets/theme-builder/src/ThemeBuilderRouter.js
@@ -5,20 +5,10 @@ import Categories from './components/Categories';
 function ThemeBuilderRouter({ categoriesProps, categoryData }) {
 	const query = new URLSearchParams( useLocation().search );
 	const page = query.get( 'page' );
-	const path = query.get( 'path' );
-
 	let routePage = <p> Fallback Route Page </p>;
 	if ( 'theme-builder' === page ) {
-        switch ( path ) {
-            case 'all-layouts':
-                routePage = <Categories items={categoriesProps} />;
-                break;
-            default:
-                routePage = <Categories items={categoriesProps} />;
-                break;
-        }
+        routePage = <Categories items={categoriesProps} />;
 	}
-
 	return <>{ routePage }</>;
 }
 
